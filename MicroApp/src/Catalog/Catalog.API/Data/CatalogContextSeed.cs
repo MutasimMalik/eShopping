177c63ae7d3361a9dc651caba9b758ecea237ee6
Mutasim Malik <malikmutasim2121@gmail.com>
2021-04-09 12:25:33 +0600

    CatalogAPI in progress

---
+++ MicroApp/src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,82 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreConfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "IPhone 12 Pro",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Samsung Galaxy S21",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "product-2.png",
+                    Price = 840.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Huawei P50 Pro",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "product-3.png",
+                    Price = 650.00M,
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Name = "Xiaomi Mi 11 Pro",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "product-4.png",
+                    Price = 470.00M,
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Name = "Xperia 1 III",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "product-5.png",
+                    Price = 380.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "LG G8 ThinQ",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "product-6.png",
+                    Price = 240.00M,
+                    Category = "Home Kitchen"
+                }
+            };
+        }
+    }
+}
