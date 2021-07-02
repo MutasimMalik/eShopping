9b020ef133de2c4bbd9bbc7af65e966dc55f762a
Mutasim Malik <malikmutasim2121@gmail.com>
2021-07-02 17:14:44 +0600

    Building the clean architecture of Ordering service

---
+++ MicroApp/src/Ordering/Ordering.Core/Entities/Base/EntityBase.cs
@@ -0,0 +1,49 @@
+﻿namespace Ordering.Core.Entities.Base
+{
+    public abstract class EntityBase<TId> : IEntityBase<TId>
+    {
+        public TId Id { get; protected set; }
+
+        public bool IsTransient()
+        {
+            return Id.Equals(default(TId));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is EntityBase<TId>))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            var item = (EntityBase<TId>)obj;
+
+            if (item.IsTransient() || IsTransient())
+                return false;
+            else
+                return item == this;
+        }
+
+        public static bool operator ==(EntityBase<TId> left, EntityBase<TId> right)
+        {
+            if (Equals(left, null))
+                return Equals(right, null) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(EntityBase<TId> left, EntityBase<TId> right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
